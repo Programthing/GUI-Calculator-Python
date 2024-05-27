22a921e6fefc146434605427c7e24215862893b5
Programthing <168237154+Programthing@users.noreply.github.com>
2024-05-27 18:06:20 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -31,6 +31,7 @@ def clear_field():
 root = tk.Tk()
 root.geometry('300x275')
 root.resizable(False, False)
+root.title('GUI Calculator')
 text_result = tk.Text(root, height = 2, width = 16, font = ('Arial', 24))
 text_result.grid(columnspan = 5)
 
